ad81bfe2816416b2e2796ebd25071bc9908c309d
shagufa01 <shagufanaaz321@gmail.com>
2020-03-08 11:05:27 +0530

    form

---
+++ script.js
@@ -0,0 +1,8 @@
+function ValidFirstName() {
+    var FirstName = document.getElementById("f-n").Value;
+    if (FirstName ="") {
+        document.getElementById("para").innerHTML("first name cannot be empty");
+    } else {
+        
+    }
+}
